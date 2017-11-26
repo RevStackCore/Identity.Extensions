40d904235cce605852e0d1bf76a56080c1a9c755
Scott <sfrancis@misinteractive.com>
2017-11-26 14:11:28 -0500

    Updated to Identity2 and netstandard2.0

--- RevStackCore.Identity.Extensions/Managers/RoleManager.cs
+++ RevStackCore.Identity.Extensions/Managers/RoleManager.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Http;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
 
@@ -9,7 +7,7 @@ namespace RevStackCore.Identity.Extensions
     public class IdentityRoleManager<TRole,TKey> : RoleManager<TRole> where TRole : class, IIdentityRole<TKey>
     {
         public IdentityRoleManager(IIdentityRoleStore<TRole, TKey> store, IEnumerable<IRoleValidator<TRole>> roleValidators, ILookupNormalizer normalizer, 
-                                   IdentityErrorDescriber errorDescriber,ILogger<RoleManager<TRole>> logger, IHttpContextAccessor contextAccessor) 
-            : base(store,roleValidators,normalizer,errorDescriber,logger,contextAccessor) { }
+                                   IdentityErrorDescriber errorDescriber,ILogger<RoleManager<TRole>> logger) 
+            : base(store,roleValidators,normalizer,errorDescriber,logger) { }
     }
 }
